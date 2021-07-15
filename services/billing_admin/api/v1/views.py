7b626576329621c6b6f721c2950dd280ba56b090
gid <gidexmo@protonmail.com>
2021-07-16 03:16:19 +0800

    [billing] add notify service

--- services/billing_admin/api/v1/views.py
+++ services/billing_admin/api/v1/views.py
@@ -1,5 +1,8 @@
 from django.http import JsonResponse
 
+from services.notify import send_payment_notify
+
 
 def debug(request):
+    send_payment_notify(request.scope['user_id'], request.scope['email'], '100 руб.', 'Оплата произведена успешно')
     return JsonResponse(request.scope)
