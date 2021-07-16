1cf95d62b4acd7d3bc4d49134aef098d48caf2c1
gid <gidexmo@protonmail.com>
2021-07-16 18:43:58 +0800

    [billing] fix params

--- services/billing_admin/api/v1/views.py
+++ services/billing_admin/api/v1/views.py
@@ -4,5 +4,6 @@ from services.notify import send_payment_notify
 
 
 def debug(request):
-    send_payment_notify(request.scope['user_id'], request.scope['email'], '100 руб.', 'Оплата произведена успешно')
+    print('debug')
+    send_payment_notify(request.scope['user_id'], '100 руб.', 'Оплата произведена успешно')
     return JsonResponse(request.scope)
