ffee1dd5dc64d460c24aed17e92619ed80f71593
Alexey Matuynin <amatuynin@aligntech.com>
2021-07-20 10:47:47 +0300

    make order #18

--- services/billing_admin/config/celery.py
+++ services/billing_admin/config/celery.py
@@ -1,6 +1,5 @@
 import os
 from celery import Celery
-from django.apps import apps
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
 
@@ -14,8 +13,3 @@ def debug_task():
     print('celery debug task!')
 
 
-@app.task(queue="high", timeout=10, default_retry_delay=10, max_retries=30)
-def wait_payment_task(payment_id):
-    payment_model = apps.get_model('subscriptions', 'PaymentInvoice')
-    pay = payment_model.objects.filter(id=payment_id).first()
-    pay.payment_system_instance.check_payment_status()
