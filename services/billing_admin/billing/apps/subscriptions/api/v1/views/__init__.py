ce3053d37f28c51d83fcdf4466619116c90653a3
gid <gidexmo@protonmail.com>
2021-07-30 01:02:11 +0800

    fix imports

--- services/billing_admin/billing/apps/subscriptions/api/v1/views/__init__.py
+++ services/billing_admin/billing/apps/subscriptions/api/v1/views/__init__.py
@@ -1,9 +1,9 @@
 from .payment import make_order
 from .product import ProductListApi
 from .subscriptions import UserSubscriptionsApi, UserSubscriptionDetailApi, UserUnsubscribeApi
-from .tariffs import TariffDetailApi, TariffListApi
+from .tariffs import TariffDetailApi
 
 __all__ = [
     'make_order', 'ProductListApi', 'UserSubscriptionsApi', 'UserSubscriptionDetailApi', 'UserUnsubscribeApi',
-    'TariffDetailApi', 'TariffListApi',
+    'TariffDetailApi',
 ]
