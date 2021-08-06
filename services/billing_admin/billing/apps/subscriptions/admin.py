b17a6b1504bc45d8f244c35def28e1aceec341ac
Alexey Matuynin <amatuynin@aligntech.com>
2021-08-06 12:33:41 +0300

    Billing admin #6

--- services/billing_admin/billing/apps/subscriptions/admin.py
+++ services/billing_admin/billing/apps/subscriptions/admin.py
@@ -11,7 +11,7 @@ class PaymentsInline(admin.TabularInline):
     verbose_name = 'платеж'
     verbose_name_plural = 'платежи'
     show_change_link = True
-    readonly_fields = ('id', 'amount', 'status', 'payment_system')
+    readonly_fields = ('created', 'id', 'amount', 'status', 'payment_system')
     fields = ('created', 'id', 'amount', 'status', 'payment_system')
 
 
